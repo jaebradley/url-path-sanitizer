1e4ff37c78be576846c5f691ba8b87a738e15c02
Jae Bradley <jae.b.bradley@gmail.com>
2020-06-09 18:39:58 -0400

    fix(dependencies): update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import localResolve from 'rollup-plugin-local-resolve';
 import filesize from 'rollup-plugin-filesize';
-import minify from 'rollup-plugin-babel-minify';
 import globals from 'rollup-plugin-node-globals';
 import builtins from 'rollup-plugin-node-builtins';
 import {
@@ -17,25 +16,31 @@ const config = {
       file: 'build/index.js',
       format: 'umd',
       name: 'url-path-sanitizer',
-      globals: ['has-a-digit'],
+      globals: {
+        'has-a-digit': 'hasADigit',
+      },
     },
     {
       file: 'build/index.cjs.js',
       format: 'cjs',
       name: 'url-path-sanitizer',
-      globals: ['has-a-digit'],
+      globals: {
+        'has-a-digit': 'hasADigit',
+      },
     },
     {
       file: 'build/index.esm.js',
       format: 'es',
-      globals: ['has-a-digit'],
+      globals: {
+        'has-a-digit': 'hasADigit',
+      }
     },
   ],
   external: ['has-a-digit'],
   plugins: [
     globals(),
     builtins(),
-    babel({ exclude: 'node_modules/**' }),
+    babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
     localResolve(),
     resolve({
       module: true,
@@ -45,7 +50,6 @@ const config = {
       browser: true,
       modulesOnly: true,
     }),
-    minify(),
     terser(),
     commonjs(),
     filesize(),
